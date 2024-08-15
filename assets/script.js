231b12d504226e9780075a1d906b907492b8037f
sieke13 <gera.ave2@gmail.com>
2024-08-15 12:32:37 -0600

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -24,7 +24,6 @@ function selectTopic() {
 console.log('Here are the topics we learned through Prework:');
 listTopics();
 console.log('Which topic should we study first?');
-
 console.log('Here are the topics we learned through Prework:');
 listTopics();
 console.log('Which topic should we study first?');
